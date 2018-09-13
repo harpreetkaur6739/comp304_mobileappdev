8355e006f7fbc157c5863f324aba9dc0d43102e0
harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2018-09-13 10:20:41 -0400

    Changes

--- IntentApplication/app/src/main/java/com/example/a300910377/intentapplication/FirstActivity.java
+++ IntentApplication/app/src/main/java/com/example/a300910377/intentapplication/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.a300910377.intentapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void moveActivity(View v){
+        Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+        startActivity(i);
     }
 }
