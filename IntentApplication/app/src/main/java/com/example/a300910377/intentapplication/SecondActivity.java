b9d423031cb7cdfe920779bec63cad78e6ad51e5
harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2018-09-13 10:03:27 -0400

    Week2 Intent Application Example

---
+++ IntentApplication/app/src/main/java/com/example/a300910377/intentapplication/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.a300910377.intentapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
